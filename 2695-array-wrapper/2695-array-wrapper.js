80f31781f5211fbd49a0cc8c0e9bef0a45f38740
Ayush <ak4345230@gmail.com>
2026-08-07 08:14:24 +0530

    Time: 24 ms (99.82%), Space: 53.6 MB (82.04%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,20 @@
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+    this.s = nums.reduce((a, b) => a + b, 0);
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.s;
+};
+
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums}]`;
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * console.log(obj1 + obj2);   // 10
+ * console.log(String(obj1));  // "[1,2]"
+ * console.log(String(obj2));  // "[3,4]"
+ */
